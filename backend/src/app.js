526576cf3d5d7197bf18706224b63f1c2e806ad5
Marcio Bispo <91513221+EngMarcioBispo@users.noreply.github.com>
2022-09-05 13:55:25 -0300

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,7 @@ const rotas = require('./router');
 const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 3003;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
