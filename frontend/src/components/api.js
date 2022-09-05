f53822b668f0c92483653143f023d54d966fb3e9
Marcio Bispo <91513221+EngMarcioBispo@users.noreply.github.com>
2022-09-05 15:16:37 -0300

    Update api.js

--- frontend/src/components/api.js
+++ frontend/src/components/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const api = axios.create({
-  baseURL:'http://localhost:3003/divisores/'   
+  baseURL:'https://divisores-primos-api.herokuapp.com/divisores/'   
 })
 
 
